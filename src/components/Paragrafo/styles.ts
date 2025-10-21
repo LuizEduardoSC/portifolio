44031d573902b0aa756b1b37efa1314b5f068da6
LuizEduardoSC <luizeduardoedd1@gmail.com>
2025-10-21 20:28:07 -0300

    Atualizando o tema e adicionado o tema dark

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,9 +1,22 @@
 import styled from 'styled-components'
 
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    corPrincipal: string
+    corSecundaria: string
+    corDeFundo: string
+    corDeFundoBotao: string
+    corDaBorda: string
+  }
+}
+
 import { Props } from './index'
 
 export const P = styled.p<Props>`
   font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
-  color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
+  color: ${(props) =>
+    props.tipo === 'principal'
+      ? props.theme.corPrincipal
+      : props.theme.corSecundaria};
   line-height: 22px;
 `
